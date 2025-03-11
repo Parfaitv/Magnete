cc86800e07e454bf5c60f90ccf399bdc91ad7ef5
parfaitv <parfaitv@student.21-school.ru>
2025-03-11 21:46:45 +0300

    feat: correct catalog page

--- src/page/Catalog.tsx
+++ src/page/Catalog.tsx
@@ -1,4 +1,4 @@
-import { FlexBox, GridBox, TextManrope } from "@/components";
+import { FlexBox, FlexBoxParts, GridBox, TextManrope } from "@/components";
 import { Link, useNavigate } from "react-router-dom";
 import { useAppSelector } from "@/store";
 import { useCSSMedia } from "@/utils";
@@ -21,44 +21,47 @@ export const Catalog = () => {
         Каталог
       </TextManrope>
       <GridBox gridTemplateColumns="1fr" gap="1rem">
-        {catalogState.map(({ items, navPath, title }, i) => (
-          <GridBox key={`${title}_${i}`} gridTemplateColumns="1fr" gap="1rem">
+        {catalogState.map(({ items, navPath: titlePath, title }, i) => (
+          <GridBox gridTemplateColumns="1fr" gap="1rem">
             <TextManrope
+              onClick={() => navigate(titlePath)}
               style={{
                 color: "black",
                 textDecoration: "none",
                 fontSize: 32,
                 fontWeight: "100",
               }}
-              onClick={() => navigate(navPath)}
             >
               {title}
             </TextManrope>
-            <GridBox gridTemplateColumns={"repeat(3,1fr)"}>
+            <GridBox gridTemplateColumns={`repeat(${isMobile ? 1 : 3}, 1fr)`}>
               {items.map(({ img, navPath }, k) => (
-                <Link
-                  key={`${img}_${navPath}_${k}`}
-                  to={navPath}
-                  style={{
-                    width: "100%",
-                    height: isMobile ? "720px" : "100%",
-                    color: "black",
-                    textDecoration: "none",
-                    fontSize: 32,
-                    fontWeight: "100",
-                  }}
-                >
-                  <FlexBox
-                    width="100%"
-                    height="960px"
-                    sx={{
-                      backgroundImage: `url(${img})`,
-                      backgroundSize: "100% 100%",
-                      backgroundRepeat: "no-repeat",
-                      backgroundPosition: "center",
+                <GridBox key={`${img}_${navPath}_${k}`}>
+                  <Link
+                    to={navPath}
+                    style={{
+                      display: "flex",
+                      flexDirection: "column",
+                      width: "100%",
+                      height: isMobile ? "720px" : "100%",
+                      color: "black",
+                      textDecoration: "none",
+                      fontSize: 32,
+                      fontWeight: "100",
                     }}
-                  />
-                </Link>
+                  >
+                    <FlexBox
+                      width="100%"
+                      height="960px"
+                      sx={{
+                        backgroundImage: `url(${img})`,
+                        backgroundSize: "100% 100%",
+                        backgroundRepeat: "no-repeat",
+                        backgroundPosition: "center",
+                      }}
+                    />
+                  </Link>
+                </GridBox>
               ))}
             </GridBox>
           </GridBox>
@@ -67,3 +70,56 @@ export const Catalog = () => {
     </GridBox>
   );
 };
+/**
+ * items.length < 3 ? (
+            <FlexBoxParts
+              key={`${i}_${title}`}
+              header={{ navPath, title }}
+              items={items}
+            />
+          ) : (
+            <GridBox key={`${title}_${i}`} gridTemplateColumns="1fr" gap="1rem">
+              <TextManrope
+                style={{
+                  color: "black",
+                  textDecoration: "none",
+                  fontSize: 32,
+                  fontWeight: "100",
+                }}
+                onClick={() => navigate(navPath)}
+              >
+                {title}
+              </TextManrope>
+              <GridBox gridTemplateColumns={"repeat(3,1fr)"}>
+                {items.map(({ img, navPath }, k) => (
+                  <GridBox key={`${img}_${navPath}_${k}`}>
+                    <Link
+                      to={navPath}
+                      style={{
+                        display: "flex",
+                        flexDirection: "column",
+                        width: "100%",
+                        height: isMobile ? "720px" : "100%",
+                        color: "black",
+                        textDecoration: "none",
+                        fontSize: 32,
+                        fontWeight: "100",
+                      }}
+                    >
+                      <FlexBox
+                        width="100%"
+                        height="960px"
+                        sx={{
+                          backgroundImage: `url(${img})`,
+                          backgroundSize: "100% 100%",
+                          backgroundRepeat: "no-repeat",
+                          backgroundPosition: "center",
+                        }}
+                      />
+                    </Link>
+                  </GridBox>
+                ))}
+              </GridBox>
+            </GridBox>
+          )
+ */
